06a826a4839fb3361b8872e099525c83186be02b
ben <huang831206@gmail.com>
2017-12-25 21:05:37 +0800

    init

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.webpackConfig({
+ 	resolve: {
+ 		alias: {
+ 			jquery: "jquery/src/jquery",
+            handlebars: 'handlebars/dist/handlebars.min.js'
+ 		}
+ 	}
+ });
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .extract([
+        'handlebars',
+        'underscore',
+        'jquery',
+        'axios',
+        'vue',
+        './resources/assets/semantic/dist/semantic'])
+   .sass('resources/assets/sass/app.scss', 'public/css');
