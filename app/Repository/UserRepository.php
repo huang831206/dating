b7f149b215d87305a5aaddd7a1bed2bffcd545fd
ben <huang831206@gmail.com>
2018-01-01 01:02:58 +0800

    get user matches data

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\User;
 
 
 class UserRepository
@@ -11,6 +12,40 @@ class UserRepository
 
     public static function getUserMatches($user_id)
     {
-        return DB::table('mathes')->where('user_a_id', $user_id)->orWhere('user_b_id', $user_id)->get();
+        // TODO:determine who the rating was rated by
+        $matches = DB::table('matches')
+                    // ->select('hash', 'rating_a', 'rating_b', 'created_at', 'updated_at')
+                    ->where('user_a_id', $user_id)
+                    ->orWhere('user_b_id', $user_id)
+                    ->get();
+
+        foreach ($matches as $match) {
+            if ($user_id == $match->user_a_id) {    // in this match, user is a
+                $match->me_rated = $match->rating_a;
+                $match->i_rate = $match->rating_b;
+                $match->other_profile = User::find($match->user_b_id)->profile;
+            } else {                                // user is b
+                $match->me_rated = $match->rating_b;
+                $match->i_rate = $match->rating_b;
+                $match->other_profile = User::find($match->user_a_id)->profile;
+            }
+            unset($match->id);
+            unset($match->user_a_id);
+            unset($match->user_b_id);
+            unset($match->rating_a);
+            unset($match->rating_b);
+            unset($match->other_profile->id);
+            unset($match->other_profile->user_id);
+            unset($match->other_profile->created_at);
+            unset($match->other_profile->updated_at);
+        }
+
+        return $matches;
+    }
+
+    public function setCurrentMatchHash(User $user, $hash)
+    {
+        $user->hash = $hash;
+        $user->save();
     }
 }
